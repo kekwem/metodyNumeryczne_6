3d7d6aa984f13b8bcdc72cbda95629aa6cfde9d7
kekwem <93526125+kekwem@users.noreply.github.com>
2022-04-02 16:32:03 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace rozniczkowanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelTest = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.text3 = new System.Windows.Forms.TextBox();
+            this.text4 = new System.Windows.Forms.TextBox();
+            this.text5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "h";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelTest
+            // 
+            this.labelTest.AutoSize = true;
+            this.labelTest.Location = new System.Drawing.Point(234, 133);
+            this.labelTest.Name = "labelTest";
+            this.labelTest.Size = new System.Drawing.Size(31, 17);
+            this.labelTest.TabIndex = 5;
+            this.labelTest.Text = "f\'(x)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "f\'\'(x)";
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(12, 79);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(100, 22);
+            this.text1.TabIndex = 7;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(165, 79);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(100, 22);
+            this.text2.TabIndex = 8;
+            // 
+            // text3
+            // 
+            this.text3.Location = new System.Drawing.Point(323, 79);
+            this.text3.Name = "text3";
+            this.text3.Size = new System.Drawing.Size(100, 22);
+            this.text3.TabIndex = 9;
+            // 
+            // text4
+            // 
+            this.text4.Location = new System.Drawing.Point(481, 79);
+            this.text4.Name = "text4";
+            this.text4.Size = new System.Drawing.Size(100, 22);
+            this.text4.TabIndex = 10;
+            // 
+            // text5
+            // 
+            this.text5.Location = new System.Drawing.Point(619, 79);
+            this.text5.Name = "text5";
+            this.text5.Size = new System.Drawing.Size(100, 22);
+            this.text5.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(118, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "x^4 +";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(271, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "x^3 +";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(429, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "x^2 +";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(587, 79);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "x +";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 473);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.text5);
+            this.Controls.Add(this.text4);
+            this.Controls.Add(this.text3);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelTest);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Rozniczkowanie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelTest;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.TextBox text3;
+        private System.Windows.Forms.TextBox text4;
+        private System.Windows.Forms.TextBox text5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rozniczkowanie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double funkcja(double x)
+        {
+            //x^4 + x^3 + x^2 + x + var
+            return Convert.ToDouble(text1.Text) * Math.Pow(x, 4) + Convert.ToDouble(text2.Text) * Math.Pow(x, 3) +
+                Convert.ToDouble(text3.Text) * Math.Pow(x, 2) + Convert.ToDouble(text4.Text) * x + Convert.ToDouble(text5.Text);
+            //return Math.Round(Math.Pow(x, 2) + 4 * x - 5, 2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text += "\nDwupunktowe roznice zwykle\nTrzypunktowe roznice zwykle\nDwupunktowe roznice wsteczne" +
+                "\nTrzypunktowe roznice wsteczne\nDwupunktowe roznice centralne\nCzteropunktowe roznice zwykle";
+            label5.Text += "\nTrzypunktowe roznice zwykle\nTrzypunktowe roznice wsteczne\nTrzypunktowe roznice centralne" +
+                "\nPieciopunktowe roznice centralne";
+
+
+            double x = Convert.ToDouble(textBox1.Text);
+            double h = Convert.ToDouble(textBox2.Text);
+
+            double[] tablica_x = new double[5];
+            double liczba_testowa = x - 2 * h;
+            for (int j = 0; j < tablica_x.Length; j++)
+            {
+                tablica_x[j] = Math.Round(liczba_testowa + j * h, 1);
+            }
+
+            double[] f_x = new double[5];
+            for (int j = 0; j < f_x.Length; j++)
+            {
+                f_x[j] = funkcja(tablica_x[j]);
+            }
+
+            int i = 2;
+            double[] pierwsz = new double[6];
+            pierwsz[0] = (f_x[i + 1] - f_x[i]) / h;
+            pierwsz[1] = (-3 * f_x[i] + 4 * f_x[i + 1] - f_x[i + 2]) / (2 * h);
+            pierwsz[2] = (f_x[i] - f_x[i - 1]) / h;
+            pierwsz[3] = (f_x[i - 2] - 4 * f_x[i - 1] + 3 * f_x[i]) / (2 * h);
+            pierwsz[4] = (f_x[i + 1] - f_x[i - 1]) / (2 * h);
+            pierwsz[5] = (f_x[i - 2] - 8 * f_x[i - 1] + 8 * f_x[i + 1] - f_x[i + 2]) /( 12 * h);
+
+            double[] druga = new double[4];
+            druga[0] = (f_x[i] - 2 * f_x[i + 1] + f_x[i + 2]) / (Math.Pow(h, 2));
+            druga[1] = (f_x[i - 2] - 2 * f_x[i - 1] + f_x[i]) / (Math.Pow(h, 2));
+            druga[2] = (f_x[i - 1] - 2 * f_x[i] + f_x[i + 1]) / (Math.Pow(h, 2));
+            druga[3] = (-f_x[i-2] + 16 * f_x[i-1] - 30 * f_x[i] + 16 * f_x[i+1] - f_x[i+2]) / (12 * Math.Pow(h, 2));
+
+            for (int j = 0; j< pierwsz.Length; j++)
+            {
+                labelTest.Text += "\n" + pierwsz[j];
+            }
+
+            for (int j = 0; j < druga.Length; j++)
+            {
+                label3.Text += "\n" + druga[j];
+            }
+        }
+    }
+}
